763e4f1f54b701afb11f95012897c919e8c9c5c8
Graren <oscar_col15@hotmail.com>
2017-05-25 20:18:08 -0400

    Deleted startup call from root container

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -2,19 +2,11 @@ import React, { Component } from 'react'
 import { View, StatusBar } from 'react-native'
 import NavigationRouter from '../Navigation/NavigationRouter'
 import { connect } from 'react-redux'
-import StartupActions from '../Redux/StartupRedux'
-import ReduxPersist from '../Config/ReduxPersist'
 
 // Styles
 import styles from './Styles/RootContainerStyles'
 
 class RootContainer extends Component {
-  componentDidMount () {
-    // if redux persist is not active fire startup action
-    if (!ReduxPersist.active) {
-      this.props.startup()
-    }
-  }
 
   render () {
     return (
